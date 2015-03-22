71a48d9fe48027b229638b56b5f99231de471f53
leeper <thosjleeper@gmail.com>
2015-03-22 17:59:01 +0100

    check messages for duplicates (closes #4)

---
+++ R/duplicates.R
@@ -0,0 +1,21 @@
+find_msg_duplicates <- function(pkg = ".") {
+    pkg <- as.package(pkg)
+    msgs <- unlist(get_messages(pkg = pkg))
+    locations <- names(msgs) <- basename(names(msgs))
+    locations <- gsub("[[:digit:]]+$", "", locations)
+    distance <- sapply(msgs, adist, x = msgs)
+    colnames(distance) <- NULL
+    matches <- sapply(msgs, function(z) agrep(z, x = msgs[msgs != z]))
+    structure(list(Message = unname(msgs), Location = locations, Matches = matches, Distance = distance), class = "msg_duplicates")
+}
+
+print.msg_duplicates <- function(x, ...) {
+    d <- numeric(nrow(x$Distance))
+    for(i in 1:nrow(x$Distance))
+        d[i] <- min(x$Distance[i,-i])
+    print(data.frame(Message = x$Message, Location = x$Location, 
+                     Matches = sapply(x$Matches, function(z) if(length(z)) paste(z, collapse = ",") else "None"),
+                     MinDistance = d,
+                     stringsAsFactors = FALSE), right = FALSE, row.names = FALSE)
+    invisible(x)
+}
