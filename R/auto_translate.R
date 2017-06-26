1e24ad6569cf6e2edad2145b559544dbd399fead
Richard Cotton <richierocks@gmail.com>
2017-06-25 22:09:25 -0400

    use autotranslate

--- R/auto_translate.R
+++ R/auto_translate.R
@@ -9,6 +9,8 @@
 #' translation will be translated.
 #' @param api_key A string describing an API key for Google Translate or Microsoft 
 #' Translator.
+#' @param parallelization_strategy A string naming a parallelization strategy,
+#' passed to \code{\link[future]{plan}}.
 #' @return An object of class \code{"po"} containing a message translation.
 #' @note To get a Google Translate API KEY, you must create a Google Cloud
 #' account. See \url{https://cloud.google.com/translate/docs/getting-started}
@@ -26,12 +28,21 @@
 #'   # generate Spanish translation in memory
 #'   (tran <- make_translation("es", translator = "Some Person <example@example.com>"))
 #'   
-#'   auto_tran <- google_translate(tran)
-#'   View(auto_tran$direct)
-#'   View(auto_tran$countable)
+#'   # You need a Google Translate API key before running this
+#'   auto_tran_google <- google_translate(tran)
+#'   View(auto_tran_google$direct)
+#'   View(auto_tran_google$countable)
+#'   
+#'   # You need a Microsoft Cognitive Services Translator API 
+#'   # key before running this
+#'   auto_tran_microsoft <- microsoft_translate(tran)
+#'   View(auto_tran_microsoft$direct)
+#'   View(auto_tran_microsoft$countable)
 #' }
-auto_translate <- function(translation, overwrite = FALSE, api_key, 
-                           engine = c("google", "microsoft")) {
+#' @importFrom autotranslate get_translations
+auto_translate <- function(translation, overwrite = FALSE, api_key,
+    parallelization_strategy = c("sequential", "multicore", "cluster"),
+    engine = c("google", "microsoft")) {
   engine <- match.arg(engine)
   
   to_translate <- if(overwrite) {
@@ -39,11 +50,12 @@ auto_translate <- function(translation, overwrite = FALSE, api_key,
   } else {
     translation$direct$msgstr == ""
   }
-  direct_translations <- translate(
-    content.vec = translation$direct$msgid[to_translate],
-    google.api.key = api_key,
-    source.lang = "en",
-    target.lang = translation$lang
+  direct_translations <- get_translations(
+    x = translation$direct$msgid[to_translate],
+    lang_to = translation$lang,
+    lang_from = "en",
+    api_key = api_key,
+    engine = engine
   )  
   translation$direct$msgstr[to_translate] <- direct_translations
   
@@ -62,18 +74,20 @@ auto_translate <- function(translation, overwrite = FALSE, api_key,
       logical(1L)
     )
   }
-  singular_countable_translations <- translate(
-    content.vec = translation$countable$msgid[to_translate],
-    google.api.key = api_key,
-    source.lang = "en",
-    target.lang = translation$lang
+  singular_countable_translations <- get_translations(
+    x = translation$countable$msgid[to_translate],
+    lang_to = translation$lang,
+    lang_from = "en",
+    api_key = api_key,
+    engine = engine
   )
   plural_countable_translations <- if (translation$n_plural_forms > 1L) {
-    translate(
-      content.vec = translation$countable$msgid_plural[to_translate],
-      google.api.key = api_key,
-      source.lang = "en",
-      target.lang = translation$lang
+    get_translations(
+      x = translation$countable$msgid_plural[to_translate],
+      lang_to = translation$lang,
+      lang_from = "en",
+      api_key = api_key,
+      engine = engine
     )  
   } else {
     character(sum(to_translate))
@@ -92,57 +106,25 @@ auto_translate <- function(translation, overwrite = FALSE, api_key,
 #' @rdname auto_translate
 #' @export
 google_translate <- function(translation, overwrite = FALSE, 
-                             api_key = Sys.getenv("GOOGLE_TRANSLATE_API_KEY")) {
-  auto_translate(translation, overwrite = overwrite, api_key, engine = "google")
+    api_key = Sys.getenv("GOOGLE_TRANSLATE_API_KEY"),
+    parallelization_strategy = c("sequential", "multicore", "cluster")) {
+  auto_translate(
+    translation, 
+    overwrite = overwrite, 
+    api_key = api_key,
+    engine = "google"
+  )
 }
 
 #' @rdname auto_translate
 #' @export
 microsoft_translate <- function(translation, overwrite = FALSE, 
-                                api_key = Sys.getenv("MICROSOFT_TRANSLATER_API_KEY")) {
-  auto_translate(translation, overwrite = overwrite, api_key, engine = "microsoft")
+    api_key = Sys.getenv("MICROSOFT_TRANSLATOR_API_KEY"),
+    parallelization_strategy = c("sequential", "multicore", "cluster")) {
+  auto_translate(
+    translation, 
+    overwrite = overwrite, 
+    api_key = api_key, 
+    engine = "microsoft"
+  )
 }
-
-# translateR::translate() is currently broken.
-# Maintainers contacted and awaiting a fix.
-# In the meantime, this is the same function with lapply() instead of mclapply().
-translate <- function (dataset = NULL, content.field = NULL, content.vec = NULL, 
-  google.api.key = NULL, microsoft.client.id = NULL, microsoft.client.secret = NULL, 
-  source.lang = NULL, target.lang = NULL) {
-  translator <- translateR:::validateInput(dataset, content.field, content.vec, 
-    google.api.key, microsoft.client.id, microsoft.client.secret, 
-    source.lang, target.lang)
-  if (!(is.null(dataset))) {
-    to.translate <- dataset[[content.field]]
-  }
-  if (!(is.null(content.vec))) {
-    to.translate <- content.vec
-  }
-  translateR:::checkLang(to.translate, source.lang, translator)
-  if (translator == "Google") {
-    translated <- unname(unlist(lapply(to.translate, function(x) translateR:::googleTranslate(x, 
-      google.api.key, source.lang, target.lang))))
-  }
-  if (translator == "Microsoft") {
-    ptm <- proc.time()
-    access.token <- getAccessToken(microsoft.client.id, microsoft.client.secret)
-    translated <- c()
-    for (doc in to.translate) {
-      translated <- c(translated, microsoftTranslate(doc, 
-        access.token, source.lang, target.lang))
-      if ((proc.time() - ptm)[3] > 540) {
-        ptm <- proc.time()
-        access.token <- getAccessToken(microsoft.client.id, 
-          microsoft.client.secret)
-      }
-    }
-  }
-  if (!(is.null(content.vec))) {
-    return(translated)
-  }
-  if (!(is.null(dataset) & is.null(content.field))) {
-    dataset$translatedContent <- translated
-    return(dataset)
-  }
-}
-
